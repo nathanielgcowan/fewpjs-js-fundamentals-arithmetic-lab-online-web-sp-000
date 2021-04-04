3379f0b7d528166e281fec38c14c9a320a6c82f1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-04 02:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+
+// Write your code here
+
+num1 = 1;
+
+num2= 62;
+
+num3 = 5;
+num4 = 6;
+
+multiply = num1 * num2;
+
+random = 1;
+
+Math.random();
+mod = num 4 % num3;
+Math.max(1, 20);
